978609c6a10cd65da284a26b9e790269c3e4254a
sofcalca <10255447+sofcalca@users.noreply.github.com>
2020-05-10 17:48:40 +0200

    deleted procfile and refactored

--- text_generator/app.py
+++ text_generator/app.py
@@ -1,10 +1,8 @@
 import os
-import subprocess
-
-from flask import Flask, jsonify, request
 
 from ariadne import graphql_sync
 from ariadne.constants import PLAYGROUND_HTML
+from flask import Flask, jsonify, request
 
 from text_generator.query import schema
 
@@ -14,10 +12,6 @@ app.secret_key = os.environ.get('FLASK_SECRET', '+%+3Q23!zbc+!Dd@')
 
 @app.route("/graphql", methods=["GET"])
 def graphql_playgroud():
-    # On GET request serve GraphQL Playground
-    # You don't need to provide Playground if you don't want to
-    # but keep on mind this will not prohibit clients from
-    # exploring your API using desktop GraphQL Playground app.
     return PLAYGROUND_HTML, 200
 
 
@@ -25,8 +19,6 @@ def graphql_playgroud():
 def graphql_server():
     data = request.get_json()
 
-    # Note: Passing the request to the context is optional.
-    # In Flask, the current request is always accessible as flask.request
     success, result = graphql_sync(
         schema,
         data,
@@ -37,17 +29,6 @@ def graphql_server():
     status_code = 200 if success else 400
     return jsonify(result), status_code
 
-
-@app.route('/', methods=["GET"])
-def ping():
-    return "", 200
-
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, use_reloader=True)
-
-"""
-{
-    delire(text: "Hello")
-}
-"""
